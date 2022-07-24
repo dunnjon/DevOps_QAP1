e553eea3d3d14ce137f2ddb47695b53c7afd4d86
dunnjon <95690225+dunnjon@users.noreply.github.com>
2022-07-24 12:31:50 -0230

    commit for revert

--- another.js
+++ another.js
@@ -4,7 +4,7 @@ function another() {
   return (
     <div>
       <h1>another file via new branch</h1>
-      <h1>making 2nd commit for revert</h1>
+      <h1>making 2nd commit for reverting</h1>
     </div>
   );
 }
